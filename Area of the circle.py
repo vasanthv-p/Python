1b484e1ef15a17bad8e45b5e5d7a9268a2857294
vasanthv-p <98248194+vasanthv-p@users.noreply.github.com>
2022-01-23 10:24:57 +0530

    mycap project

---
+++ Area of the circle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 10:09:44 2022
+
+@author: Asus-2020
+"""
+
+import math as M
+r=float(input("enter the radius of the cicle : "))
+AC=M.pi*r*r
+print("Area of the circle is :",AC)
---
+++ extension.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 10:06:06 2022
+
+@author: Asus-2020
+"""
+
+filename=input("enter your FILE NAME : ")
+extn=filename.split(".")
+print("the extension of the file is : " + repr(extn[-1]))
